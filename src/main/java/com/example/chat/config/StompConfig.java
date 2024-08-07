bb148b1c5b698583a24d9ad15314e43fdc74de82
hann <wjdgks1233@gmail.com>
2024-08-07 21:55:02 +0900

    stompConfig

--- src/main/java/com/example/chat/config/StompConfig.java
+++ src/main/java/com/example/chat/config/StompConfig.java
@@ -1,33 +1,36 @@
-//package com.example.chat.config;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.messaging.simp.config.ChannelRegistration;
-//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-//import org.springframework.util.AntPathMatcher;
-//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-//import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-//import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-//
-//@Configuration
-//@EnableWebSocketMessageBroker
-//public class StompConfig implements WebSocketMessageBrokerConfigurer {
-//
-//    //endpoint를 /stomp로 하고, allowedOrigins를 "*"로 하면 페이지에서
-//    //Get /info 404 Error가 발생한다. 그래서 아래와 같이 2개의 계층으로 분리하고
-//    //origins를 개발 도메인으로 변경하니 잘 동작하였다.
-//    //이유는 왜 그런지 아직 찾지 못함
-//    @Override
-//    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/stomp/chat")
-//                .setAllowedOrigins("http://localhost:8080")
-//                .withSockJS();
-//    }
-//
-//    /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
-//    @Override
-//    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        registry.setApplicationDestinationPrefixes("/pub");
-//        registry.enableSimpleBroker("/sub");
-//    }
-//}
+package com.example.chat.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class StompConfig implements WebSocketMessageBrokerConfigurer {
+
+    //endpoint를 /stomp로 하고, allowedOrigins를 "*"로 하면 페이지에서
+    //Get /info 404 Error가 발생한다. 그래서 아래와 같이 2개의 계층으로 분리하고
+    //origins를 개발 도메인으로 변경하니 잘 동작하였다.
+    //이유는 왜 그런지 아직 찾지 못함
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/stomp/chat")
+                .setAllowedOrigins("http://localhost:8080")
+                .withSockJS();
+    }
+
+    /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setPathMatcher(new AntPathMatcher(".")); // url을 chat/room/3 -> chat.room.3으로 참조하기 위한 설정
+        registry.setApplicationDestinationPrefixes("/pub");
+
+        //registry.enableSimpleBroker("/sub");
+        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue");
+    }
+}
