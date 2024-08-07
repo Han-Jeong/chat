7905bded8af4011cde46f21f2fc2c84948c91e86
hann <wjdgks1233@gmail.com>
2024-08-07 17:32:09 +0900

    yml & rabbitConfig

--- src/main/java/com/example/chat/config/RabbitConfig.java
+++ src/main/java/com/example/chat/config/RabbitConfig.java
@@ -1,92 +1,110 @@
-//package com.example.chat.config;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.databind.SerializationFeature;
-//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.amqp.core.*;
-//import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-//import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-//import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-//import org.springframework.amqp.rabbit.core.RabbitTemplate;
-//import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-//import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//
-//@Configuration
-//@EnableRabbit
-//public class RabbitConfig {
-//
-//    private static final String CHAT_QUEUE_NAME = "chat.queue";
-//    private static final String CHAT_EXCHANGE_NAME = "chat.exchange";
-//    private static final String ROUTING_KEY = "room.*";
-//
-//    // Queue 등록
-//    @Bean
-//    public Queue queue() {
-//        return new Queue(CHAT_QUEUE_NAME, true);
-//    }
-//
-//    // Exchange 등록
-//    @Bean
-//    public TopicExchange exchange() {
-//        return new TopicExchange(CHAT_EXCHANGE_NAME);
-//    }
-//
-//    // Exchange 와 Queue 바인딩
-//    @Bean
-//    public Binding binding() {
-//        return BindingBuilder.bind(queue()).to(exchange()).with(ROUTING_KEY);
-//    }
-//
-//
-//    @Bean
-//    public  com.fasterxml.jackson.databind.Module dateTimeModule() {
-//        return new JavaTimeModule();
-//    }
-//
-//
-//    // Spring 에서 자동생성해주는 ConnectionFactory 는 SimpleConnectionFactory
-//    // 여기서 사용하는 건 CachingConnectionFactory 라 새로 등록해줌
-//    @Bean
-//    public ConnectionFactory connectionFactory() {
-//        CachingConnectionFactory factory = new CachingConnectionFactory();
-//        factory.setHost("localhost");
-//        factory.setUsername("guest");
-//        factory.setPassword("guest");
-//        return factory;
-//    }
-//
-//    /**
-//     * messageConverter를 커스터마이징 하기 위해 Bean 새로 등록
-//     */
-//
-//    @Bean
-//    public RabbitTemplate rabbitTemplate() {
-//        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
-//        rabbitTemplate.setMessageConverter(jsonMessageConverter());
-//        rabbitTemplate.setRoutingKey(CHAT_QUEUE_NAME);
-//        return rabbitTemplate;
-//    }
-//
-//    @Bean
-//    public SimpleMessageListenerContainer container() {
-//        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-//        container.setConnectionFactory(connectionFactory());
-//        container.setQueueNames(CHAT_QUEUE_NAME);
-//        container.setMessageListener(null);
-//        return container;
-//    }
-//
-//    @Bean
-//    public Jackson2JsonMessageConverter jsonMessageConverter() {
-//        //LocalDateTime serializable 을 위해
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
-//        objectMapper.registerModule(dateTimeModule());
-//        return new Jackson2JsonMessageConverter(objectMapper);
-//    }
-//}
+package com.example.chat.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EnableRabbit
+public class RabbitConfig {
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+
+    /**
+     * 1. Exchange 구성합니다.
+     * "hello.exchange" 라는 이름으로 Direct Exchange 형태로 구성하였습니다.
+     *
+     * @return DirectExchange
+     */
+    @Bean
+    DirectExchange directExchange() {
+        return new DirectExchange("hello.exchange");
+    }
+
+    /**
+     * 2. 큐를 구성합니다.
+     * "hello.queue"라는 이름으로 큐를 구성하였습니다.
+     *
+     * @return Queue
+     */
+    @Bean
+    Queue queue() {
+        return new Queue("hello.queue", false);
+    }
+
+
+    /**
+     * 3. 큐와 DirectExchange를 바인딩합니다.
+     * "hello.key"라는 이름으로 바인딩을 구성하였습니다.
+     *
+     * @param directExchange
+     * @param queue
+     * @return Binding
+     */
+    @Bean
+    Binding binding(DirectExchange directExchange, Queue queue) {
+        return BindingBuilder.bind(queue).to(directExchange).with("hello.key");
+    }
+
+
+    /**
+     * 4. RabbitMQ와의 연결을 위한 ConnectionFactory을 구성합니다.
+     * Application.properties의 RabbitMQ의 사용자 정보를 가져와서 RabbitMQ와의 연결에 필요한 ConnectionFactory를 구성합니다.
+     *
+     * @return ConnectionFactory
+     */
+    @Bean
+    ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost(host);
+        connectionFactory.setPort(port);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        return connectionFactory;
+    }
+
+    /**
+     * 5. 메시지를 전송하고 수신하기 위한 JSON 타입으로 메시지를 변경합니다.
+     * Jackson2JsonMessageConverter를 사용하여 메시지 변환을 수행합니다. JSON 형식으로 메시지를 전송하고 수신할 수 있습니다
+     *
+     * @return
+     */
+    @Bean
+    MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+
+    /**
+     * 6. 구성한 ConnectionFactory, MessageConverter를 통해 템플릿을 구성합니다.
+     *
+     * @param connectionFactory
+     * @param messageConverter
+     * @return
+     */
+    @Bean
+    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+}
